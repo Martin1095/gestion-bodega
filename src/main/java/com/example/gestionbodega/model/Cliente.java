5c6bc373bff53e27634ee02fd54c5b614a8bb8d0
Martín Maulén <mart.maulen@duocuc.cl>
2026-05-07 17:50:16 -0400

    Actualizar clientes

--- src/main/java/com/example/gestionbodega/model/Cliente.java
+++ src/main/java/com/example/gestionbodega/model/Cliente.java
@@ -1,5 +1,39 @@
 package com.example.gestionbodega.model;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(max = 50, message = "El nombre no puede tener más de 50 caracteres")
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
+    @Size(max = 50, message = "El apellido no puede tener más de 50 caracteres")
+    @Column(name = "apellido", nullable = false, length = 100)
+    private String apellido;
+
+    @NotBlank(message = "El RUT es obligatorio")
+    @Size(max = 12, message = "El RUT no puede tener más de 10 caracteres")
+    @Column(name = "rut", nullable = false, unique = true, length = 10) 
+    private String rut;
+    private String correo;
+    private String telefono;
 
 }
