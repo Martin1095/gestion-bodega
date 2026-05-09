fbb00af4c8c038d30979b7ef6f271fde455407f8
Martin1095 <mart.maulen@duocuc.cl>
2026-05-09 00:17:27 -0400

    avance de pedido

--- src/main/java/com/example/gestionbodega/repository/PedidoRepository.java
+++ src/main/java/com/example/gestionbodega/repository/PedidoRepository.java
@@ -1,5 +1,14 @@
 package com.example.gestionbodega.repository;
 
-public interface PedidoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.gestionbodega.model.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+    List<Pedido> findById_pedido(Integer id_pedido);
 }
